605e6dfbb6f21c1568109b5f787f8a6ec6027589
mishal-fatima28 <mishalfatima28@gmail.com>
2022-06-17 17:55:44 +0500

    initial commit

--- sendemail.php
+++ sendemail.php
@@ -3,13 +3,8 @@
 	$to = "mishalfatima28@gmail.com";
 	$subject = "test mail";
 	$message = "hello";
-	$from = "mishalfatima28@gmail.com";
-	$headers = "From : $from";
-	$headers =  'MIME-Version: 1.0' . "\r\n"; 
-	$headers .= 'From: Your name <mishalfatima28@gmail.com>' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-
-	mail($to, $subject, $message, $headers);
+	
+	mail($to, $subject, $message);
 	echo "mail sent";
 
 
