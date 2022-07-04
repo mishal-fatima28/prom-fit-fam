a39af14d4bd3aab3858de0b5c057d3a2e6210da4
mishal-fatima28 <mishalfatima28@gmail.com>
2022-07-04 16:47:22 +0500

    revisions

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-	// Require https
-	if ($_SERVER['HTTPS'] != "on") {
-	$url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-	header("Location: $url");
-	exit;
-	}
+	
   include_once("index.html");
 ?>
